6258148fde347d36de4b0082985742af26e91d55
steven Yabann <stevekiptoo1@gmail.com>
2023-03-16 16:10:09 +0300

    completed assignment

--- index.js
+++ index.js
@@ -9,17 +9,19 @@ const returnLastTwoDrivers = function(array){
     return array.slice(-2);
 }
 
- let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
 
- 
- let createFareMultiplier = (fare) => 
- { return (function(fare){ let ans = fare**2; return ans})}
+function createFareMultiplier(mult){
+    return (function(fare){
+        return fare* mult
+    })
+}
 
- const fareDoubler = (fare) => {return fare*2}
+const fareDoubler = createFareMultiplier(2)
 
- const fareTripler = (fare) => {return fare*3}
+const fareTripler = createFareMultiplier(3)
 
- const selectDifferentDrivers = (arg1, arg2) => {
+const selectDifferentDrivers = (arg1, arg2) => {
     if (arg2 == returnFirstTwoDrivers){
         return returnFirstTwoDrivers(arg1)
     }
